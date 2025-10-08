155b9e9cb3172e1061aeb9eab79b6eee5d3270b9
nisaninan21 <164305456+nisaninan21@users.noreply.github.com>
2025-10-08 23:25:06 +0530

    Added score board

--- FirstApplication/Assets/Scripts/PioeMoveScript.cs
+++ FirstApplication/Assets/Scripts/PioeMoveScript.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PioeMoveScript : MonoBehaviour
 {
     public float moveSpeed = 1f;
+
+    public float deadZone = -25;
+    private float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,11 @@ public class PioeMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        if ( transform.position.x < deadZone )
+        {
+            Destroy(gameObject);
+        }
     }
 }
